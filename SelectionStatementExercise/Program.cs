2b77fa8cee3698d131d397073f788d648f2e2f13
Simone Mercer <simoneblue12@gmail.com>
2025-08-24 04:35:25 -0700

    Guess my favorite number

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favNumber = 5;
+            int userInput = 0;
+
+            //Keep looping until the user guesses the correct number
+            while (userInput != favNumber)
+            {
+                Console.WriteLine("Guess my favorite number:");
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine($"Too low!\nYour guess: {userInput}");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine($"Too high!\nYour Guess: {userInput}");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it!!!\nThe favorite number was {favNumber}!");
+                }
+            }
         }
     }
 }
